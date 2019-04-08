056a9905f5275239267a8117aefdac4c0dcc3051
ValorWind1 <skyjdani@gmail.com>
2019-04-08 14:00:23 -0400

    +

--- My_PriorityQueue.py
+++ My_PriorityQueue.py
@@ -71,21 +71,3 @@ print(myQueue)
 while not myQueue.isEmpty():
     print(myQueue.delete())
 
-
-
-
-# priority queue using heapq
-
-import heapq
-
-qu = []    # python list
-                                  # (5,d5)
-heapq.heappush(qu,(5,"d5"))       # first element of the topple represents the priority and the second the data
-heapq.heappush(qu,(1,"d1"))       #
-heapq.heappush(qu,(3,"d3"))
-heapq.heappush(qu,(2,"d2"))
-heapq.heappush(qu,(4,"d4"))
-
-heapq.heappop(qu) # retriving data according to the priority
-
-print(heapq.heappop(qu))
